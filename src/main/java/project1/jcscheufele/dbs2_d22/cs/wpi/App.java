b99ee1525e63d34b1a6716211fad9a23af6098c6
jcscheufele <57052259+jcscheufele@users.noreply.github.com>
2022-03-31 08:48:08 -0400

    first commit

---
+++ src/main/java/project1/jcscheufele/dbs2_d22/cs/wpi/App.java
@@ -0,0 +1,13 @@
+package project1.jcscheufele.dbs2_d22.cs.wpi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
